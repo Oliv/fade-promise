caf08b5fc533cb6ca00dc77b9851c381fd2672cb
Oliv <jahsoul@gmail.com>
2017-11-30 14:40:50 +0100

    Initial drop

---
+++ README.md
@@ -0,0 +1,31 @@
+# fade-promise [![NPM version](https://badge.fury.io/js/fade-promise.svg)](https://npmjs.org/package/fade-promise) [![Build Status](https://travis-ci.org/Oliv/fade-promise.svg?branch=master)](https://travis-ci.org/Oliv/fade-promise)
+
+Promisified fade in es6, using requestAnimationFrame
+
+## Installation
+
+```sh
+$ npm install --save fade-promise
+```
+
+## Usage
+
+```js
+import fadePromise from 'fade-promise';
+
+fadePromise(element, to, duration).then((element, to, duration) => {
+  console.log('done !');
+});
+```
+
+## API
+
+The default `duration` is `300` ms.
+
+### fade(el, opacity[, duration])
+
+Returns a `Promise`.
+
+## License
+
+MIT © [Olivier Gasc](https://github.com/Oliv)
---
+++ index.js
@@ -0,0 +1,38 @@
+const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+export default function fade(element, to, duration = 300) {
+  if (element.style.getPropertyValue('opacity') === "")
+    element.style.setProperty('opacity', window.getComputedStyle(element).opacity || 1);
+
+  const initValue = +element.style.getPropertyValue('opacity');
+  let start = null;
+
+  const fn = (t) => (to - initValue)/duration*t + initValue;
+
+  return new Promise((resolve, reject) => {
+    if (to > 1 || to < 0)
+      reject(new Error('to parameter is out of range'));
+
+    if (duration <= 0)
+      reject(new Error('duration parameter is out of range'));
+
+    function doFade(timestamp) {
+      if (start === null)
+        start = timestamp;
+
+      const progress = timestamp - start;
+      const value = +element.style.getPropertyValue('opacity');
+
+      element.style.opacity = fn(progress);
+
+      if (progress < duration) {
+        requestAnimationFrame(doFade);
+      } else {
+        resolve(element, to, duration);
+      }
+    }
+
+    requestAnimationFrame(doFade);
+  });
+}
