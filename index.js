b6a6ccc64d780b58b9a1914144ce3712c759d5c0
Oliv <jahsoul@gmail.com>
2017-12-06 11:33:56 +0100

    Rejection if no valid element

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestA
   window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
 export default function fade(element, to, duration = 300) {
-  if (element.style.getPropertyValue('opacity') === "")
-    element.style.setProperty('opacity', window.getComputedStyle(element).opacity || 1);
-
-  const initValue = +element.style.getPropertyValue('opacity');
-  let start = null;
-
-  const linear = (t) => (to - initValue) / duration * t + initValue;
-
   return new Promise((resolve, reject) => {
+    if (element === null || element.style === undefined)
+      reject(new Error('element is not valid'));
+
     if (to > 1 || to < 0)
       reject(new Error('to parameter is out of range'));
 
     if (duration <= 0)
       reject(new Error('duration parameter is out of range'));
+
+    if (element.style.getPropertyValue('opacity') === "")
+      element.style.setProperty('opacity', window.getComputedStyle(element).opacity || 1);
+
+    const initValue = +element.style.getPropertyValue('opacity');
+    let start = null;
+
+    const linear = (t) => (to - initValue) / duration * t + initValue;
 
     function doFade(timestamp) {
       if (start === null)
